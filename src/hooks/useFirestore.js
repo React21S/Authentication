5d1717c90eafc24862c0b1299fe7c928955896a9
FemiAdesola <75606768+FemiAdesola@users.noreply.github.com>
2022-03-27 18:51:14 +0300

    Added deletedocument and delete case into useFirestore, delete button into NPSList.js

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -12,11 +12,16 @@ const firestoreReducer = (state, action) => {
   switch (action.type) {
     case 'IS_PENDING':
       return {isPending: true,document:null, success:false, error:null }
+
     case 'ADDED_DOCUMENT':
       return {isPending:false, document:action.payload, success:true, error:null}
+
+      case 'DELETED_DOCUMENT':
+        return {isPending:false, document:null, success:true, error:null}
+
     case 'ERROR':
       return {isPending:false, document:null, success:false, error:action.payload}
-      default:
+    default:
       return state
   }
 }
@@ -53,6 +58,14 @@ const addDocument = async (doc) => {
 
   // delete a document
   const deleteDocument = async (id) => {
+    dispatch({type:'IS_PENDING'});
+    try {
+    await ref.doc(id).delete();
+      dispatchIfNotCancelled({type: 'DELETED_DOCUMENT'})
+    }
+    catch(err){
+      dispatchIfNotCancelled({type: 'ERROR', payload:'could not delete'})
+    }
 
   }
 
--- src/pages/home/NPSList.js
+++ src/pages/home/NPSList.js
@@ -1,14 +1,21 @@
 import styles from "./Home.module.css";
+import useFirestore from "../../hooks/useFirestore";
 
 import React from 'react';
 
 const NPSList = ({NPScores}) => {
+    const {deleteDocument}= useFirestore('NPS')
+
+    // // To see response on console 
+    // const {deleteDocument, response}= useFirestore('NPS')
+    // console.log(response)
     return (
         <ul className={styles.NPSList}>
         {NPScores.map((NPScore) => (
           <li key={NPScore.id}>
             <p className={styles.name}>{NPScore.name}</p>
             <p className={styles.amount}>€ {NPScore.amount}</p>
+            <button onClick={()=>deleteDocument(NPScore.id)}>X</button>
           </li>
         ))}
       </ul>
